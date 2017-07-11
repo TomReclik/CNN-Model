e5cf5321a8b354760326dcde95e081c399aab0ac
Tom Reclik <tom.reclik@rwth-aachen.de>
2017-07-11 07:36:24 +0200

    Training of neural network on CIFAR-10 data and trained network

--- CPFS.py
+++ CPFS.py
@@ -1,14 +1,25 @@
+'''Train a simple deep CNN on the CIFAR10 small images dataset.
+GPU run command with Theano backend (with TensorFlow, the GPU is automatically used):
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatx=float32 python cifar10_cnn.py
+It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs.
+(it's still underfitting at that point, though).
+'''
+
+from __future__ import print_function
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
-model = Sequential()
+batch_size = 32
+num_classes = 10
+epochs = 200
+data_augmentation = True
 
-model.add(Conv2D(32, (3,3), activation='relu', input_shape(100,100,3)))
-model.add(MaxPooling2D(pool_size=2,2))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
+# The data, shuffled and split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
