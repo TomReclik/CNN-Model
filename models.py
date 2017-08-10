96d6b95c5df84f2ed5c52a0530c5cb6332cdcc5c
Tom Reclik <tom.reclik@rwth-aachen.de>
2017-08-10 19:08:17 +0200

    Numer of epochs corrected

--- models.py
+++ models.py
@@ -361,7 +361,7 @@ def Graham_Simple(x_train, y_train, x_test, y_test, NOL):
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-    model.fit(x_train, y_train, batch_size=50, epochs=60)
+    model.fit(x_train, y_train, batch_size=50, epochs=30)
     score = model.evaluate(x_test, y_test, batch_size=32)
 
     return score
