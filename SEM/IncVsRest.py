b887ca063f49c3ac3d08464fb66b8be37946f66d
Tom Reclik <tom.reclik@rwth-aachen.de>
2017-12-11 21:03:32 +0100

    Calculation of confusion matrix added. First step for hierarchical network implemented, IncVsRest.py poses as an prototype for the first categorization step

--- SEM/IncVsRest.py
+++ SEM/IncVsRest.py
@@ -5,21 +5,14 @@ import keras
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import numpy as np
-from sklearn.metrics import confusion_matrix
-
-##
-## Set seed
-##
-
-np.random.seed(42)
 
 #lang = {"Martensite":0, "Interface":1, "Boundary":2, "Evolved":3}
 # lang = {"Martensite":0, "Interface":1, "Evolved":2, "Evovled":2, "Notch":2}
 # lang = {"Martensite":0, "Interface":1}
 # lang = {"Notch":0,"Interface":1}
-lang = { "Martensite":0, "Notch":1, "Interface":2, "Boundary":3}
-class_weight = {0:1,1:1,2:1,3:1}
-dist = { "Martensite":400, "Notch":400, "Interface":400, "Boundary":115}
+lang = {"Inclusion":0, "Notch":1, "Interface":1, "Martensite":1, "Evolved":1, "Boundary":1}
+class_weight = {0:1.5,1:1}
+dist = {"Inclusion":383, "Notch":100, "Interface":100, "Martensite":100, "Evolved":100, "Boundary":100}
 
 compressedLang = []
 for key,value in lang.iteritems():
@@ -27,7 +20,7 @@ for key,value in lang.iteritems():
 compressedLang = set(compressedLang)
 NOC = len(compressedLang)
 
-name = "EERACN_SecondStage"
+name = "EERACN_IncVsAll_NoInc"
 weights_path = name + ".hdf5"
 batch_size = 5
 
@@ -71,7 +64,7 @@ score = network.evaluate(x_test, y_test, batch_size=batch_size)
 print "Accuracy on the entire test batch: ", score[1]
 
 ##
-## Test accuracy on the subclassesskmetrics
+## Test accuracy on the subclasses
 ##
 
 inv_lang = {v: k for k,v in lang.iteritems()}
@@ -93,11 +86,64 @@ for i in range(NOC):
     print "Accuracy on just the ", inv_lang[i], "test data", score[1]
     print x_tmp.shape
 
-y_pred = network.predict(x_test)
-
-y_pred = np.argmax(y_pred, axis=1)
-y_true = np.argmax(y_test, axis=1)
-
-cmatrix = confusion_matrix(y_true, y_pred)
-
-print cmatrix
+#
+# M = []
+# y_M = []
+#
+# ID = []
+# y_ID = []
+#
+# # N = []
+# # y_N = []
+#
+# # B = []
+# # y_B = []
+# #
+#
+# for i in range(y_test.shape[0]):
+#     if y_test[i][0] == 1:
+#         M.append(x_test[i])
+#         y_M.append(0)
+#     elif y_test[i][1] == 1:
+#         ID.append(x_test[i])
+#         y_ID.append(1)
+#     # elif y_test[i][2] == 1:
+#     #     N.append(x_test[i])
+#     #     y_N.append(2)
+#     # elif y_test[i][2] == 1:
+#     #     B.append(x_test[i])
+#     #     y_B.append(2)
+#
+#     # else:
+#     #     I.append(x_test[i])
+#     #     y_I.append(2)
+#
+# M = np.asarray(M, float)
+# y_M = np.asarray(y_M, int)
+# y_M = keras.utils.to_categorical(y_M, NOC)
+# score_M = network.evaluate(M, y_M, batch_size=batch_size, verbose=0)
+# print "Accuracy on just the Interface test data: ", score_M[1]
+#
+# ID = np.asarray(ID, float)
+# y_ID = np.asarray(y_ID, int)
+# y_ID = keras.utils.to_categorical(y_ID, NOC)
+# score_ID = network.evaluate(ID, y_ID, batch_size=batch_size, verbose=0)
+# print "Accuracy on just the Notch test data: ", score_ID[1]
+#
+# # B = np.asarray(B, float)
+# # y_B = np.asarray(y_B, int)
+# # y_B = keras.utils.to_categorical(y_B, NOC)
+# # score_B = network.evaluate(B, y_B, batch_size=batch_size, verbose=0)
+# # print "Accuracy on just the Boundary test data: ", score_B[1]
+# #
+# # N = np.asarray(N, float)
+# # y_N = np.asarray(y_N, int)
+# # y_N = keras.utils.to_categorical(y_N, NOC)
+# # score_N = network.evaluate(N, y_N, batch_size=batch_size, verbose=0)
+# # print "Accuracy on just the Notch test data: ", score_N[1]
+#
+# # I = np.asarray(I, float)
+# # y_I = np.asarray(y_I, int)
+# # y_I = keras.utils.to_categorical(y_I, NOC)
+# # score_I = network.evaluate(I, y_I, batch_size=batch_size)
+# # print "Accuracy on just the Inclusion test data: ", score_I[1]
